c6612dece41b11e227281963813c5fa439ff8c78
Mostafa-Code19 <tabrizian.codes@gmail.com>
2023-09-20 15:25:26 +0330

    *next.config.js add headers: x-frame-options to sameorigin x-content-type-options to nosniff strict-transport-security to 3571000 includeSubDomain preload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   async headers() {
+      return [
+         {
+            source: '/(.*)',
+            headers: [
+               {
+                  key: 'X-Frame-Options',
+                  value: 'SAMEORIGIN',
+               },
+               {
+                  key: 'X-Content-Type-Options',
+                  value: 'nosniff',
+               },
+               {
+                  key: 'Strict-Transport-Security',
+                  value: 'max-age=3571000; includeSubDomains; preload'
+               }
+            ],
+         },
+      ]
+   },
    images: {
       formats: ['image/avif', 'image/webp'],
       remotePatterns: [
