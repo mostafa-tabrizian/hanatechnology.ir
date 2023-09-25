f6c2a2b7ef880f9440043e2af87862720cbb1845
Mostafa-Code19 <tabrizian.codes@gmail.com>
2023-09-26 00:23:13 +0330

    remove last ip check in middleware test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,5 @@
-import { NextResponse, NextRequest } from 'next/server';
-
 export { default } from 'next-auth/middleware'
 
 export const config = {
    matcher: ['/--admin--/:path*', '/api/--admin--/:path*'],
 }
-
-export function middleware(request: NextRequest) {
-   const requestHeaders = new Headers(request.headers);
-
-   const ip = request.ip || '127.0.0.1';
-
-   console.log('ip', ip);
-
-   requestHeaders.set('x-forwarded-for', ip);
-
-   return NextResponse.next({
-      request: {
-         headers: requestHeaders,
-      },
-   });
-}
-
