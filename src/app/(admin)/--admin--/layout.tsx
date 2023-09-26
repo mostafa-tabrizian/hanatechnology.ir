3a4e40da44b7767ac087242aab4b07694b3f892e
Mostafa-Code19 <tabrizian.codes@gmail.com>
2023-09-26 13:47:05 +0330

    *admin/layout add no index, no follow, and no google *robots.txt remove disallow admin from public view

--- src/app/(admin)/--admin--/layout.tsx
+++ src/app/(admin)/--admin--/layout.tsx
@@ -1,3 +1,15 @@
+export const metadata = {
+   robots: {
+      index: false,
+      follow: false,
+      nocache: true,
+      googleBot: {
+         index: false,
+         follow: false,
+      },
+   },
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
    return (
       <>
